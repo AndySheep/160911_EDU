7ec3acdbe2f999b3f4c838ef8a4a3516c41bde59
AndySheep <xch5981015@163.com>
2016-09-15 03:26:27 +0800

    9.14v1.1
    修改通知条点击逻辑

--- js/edu.js
+++ js/edu.js
@@ -1,33 +1,7 @@
-/* 顶部通知条 */
-var elenote=document.getElementById('g-note');
+var note=document.getElementById('g-note');
+var closenote=document.getElementById('m-note-close');
 var shownote='shownote';
-function getElementsByClassName(element,names) {
-	if (element.getElementsByClassName) {
-		return element.getElementsByClassName(names);//特性侦测，优先使用W3C规范
-	}else{
-		var elements=element.getElementsByTagName('*'),//获取所有的后代元素
-			result=[];//建立空数组集合，用来存放识别到的符合要求的元素
-		for (var i = 0,ele,len=elements.length;ele=elements[i],i<len ;i++) {
-			if (hasClassName(ele,names)) {
-				result.push(ele);
-			}
-		}//遍历所有的后代元素，如果有符合类名的元素则将它压入result中
-		function hasClassName(elementorg,nameorg) {
-			var attrorg=elementorg.getAttribute('class');//获取具有class样式属性值
-			var attrname=nameorg.split(' ');//获取识别元素的集合
-			for (var i = 0,len=attrname.length; i < len; i++) {
-				if (attrorg.indexOf(attrname[i])) {
-					continue;
-				}else{
-					return false;
-				}
-			}
-			return true;//判断是否具有该名称属性
-		}
-		return result;
-	}
-}
-var gnotecookie=(function getcookie () {
+var notecookie=(function getcookie () {
     var cookie = {};
     var all = document.cookie;
     if (all === '')
@@ -44,10 +18,10 @@ var gnotecookie=(function getcookie () {
     }
     return cookie;
 })();
-if (gnotecookie.shownote==1) {
-	closent('elenote');console.log(gnotecookie.shownote)
-}else{
-	(function setCookie (name, value, expires, path, domain, secure) {
+function closeNote(ele) {
+	return ele.className+='closenote';
+}
+function setCookie(name, value, expires, path, domain, secure) {
     var cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
     if (expires)
         cookie += '; expires=' + expires.toGMTString();
@@ -58,11 +32,22 @@ if (gnotecookie.shownote==1) {
     if (secure)
         cookie += '; secure=' + secure;
     document.cookie = cookie;
-})(shownote,1)
 }
-var closenote=getElementsByClassName(elenote,'m-note-close');
-function closent (ele){
-	return elenote.className+='closenote';
+function addEvent(node,event,handler){
+    if (!!node.addEventListener){
+        node.addEventListener(event,handler,!1);
+    }else{
+        node.attachEvent('on'+event,handler);
+    }
 }
-// closenote.onclick=closent('elenote');
-/* /顶部通知条 */
+/* 优先判断是否含有shownote这个cookie，若有则直接关闭通知，若无则监听点击事件，监听到点击事件后先设置cookie，然后关闭通知。 */
+if (notecookie.shownote=='yes') {
+	closeNote(note);
+}else{
+	addEvent(closenote,'click',function(event){
+	setCookie(shownote,'yes');
+	})
+	addEvent(closenote,'click',function(event){
+	closeNote(note);
+	})
+}
